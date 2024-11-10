9e271101d7666373f681499efedee1569745073c
Dima <prevetlunatikam@gmail.com>
2024-11-11 00:17:09 +0300

    home

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,55 @@
-console.log(1)
 
-function addOne(age: number): number {
-    return age + 1
-   }
-   
-   const age = 'thirty tworf'
-   
-   console.log(addOne(45))
+
+interface LengData {
+    rutext: string
+    index: string
+    info_data: string
+}
+
+interface LengResult {
+    [key: string]: string
+}
+
+interface NewLengPack {
+    [key: string]: LengResult
+}
+
+const textArray: LengData[] = [
+    {rutext: 'Привет', index: 'hello', info_data: 'приветствие'},
+    {rutext: 'Назад', index: 'back', info_data: 'назад'}
+]
+
+const lengs = ['ru', 'en']
+
+const reference = {
+    hello: {
+        info_data: 'приветствие',
+        ru: 'Привет',
+        en: 'Hello'
+    },
+    back: {
+        info_data: 'назад',
+        ru: 'Назад',
+        en: 'back'
+    }
+}
+
+let result = []
+
+const getLenguagesFromOI = async (updateAll: boolean, data: LengData[], lenguages: string[]) => {
+    if(updateAll){
+        const newLengPack: NewLengPack = {}
+        for(const i of data){
+            const newRes: LengResult = {}
+            newRes.info_data = i.info_data
+            newRes.ru = i.rutext
+            for(const l of lenguages.filter(item => item !== 'ru')){
+                newRes[l] = 'dfdfdf' 
+            }
+            newLengPack[i.index] = newRes
+        }
+        console.log(newLengPack)
+    }
+}
+
+getLenguagesFromOI(true, textArray, lengs)
