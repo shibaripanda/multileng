0a0bf0633164ef481374d8dd79c3fa1e6888ff44
Dima <prevetlunatikam@gmail.com>
2024-11-10 22:03:01 +0300

    first commit

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log(1);
+function addOne(age) {
+    return age + 1;
+}
+const age = 'thirty tworf';
+console.log(addOne(45));
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+console.log(1)
+
+function addOne(age: number): number {
+    return age + 1
+   }
+   
+   const age = 'thirty tworf'
+   
+   console.log(addOne(45))
